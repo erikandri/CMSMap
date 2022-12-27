129efcd3ea4daefad318a67fc3d010839ea4f61d
Erik Andri Budiman <erik.andri10@gmail.com>
2022-12-27 16:14:56 +0700

    Update all requests to using the latest python requests instead of urllib
    Signed-off-by: Erik Andri Budiman <erik.andri10@gmail.com>

--- cmsmap/lib/requester.py
+++ cmsmap/lib/requester.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
-import http.cookiejar
-import ssl
 import urllib
 import urllib.error
 import urllib.request
 from urllib import parse
+
+import requests
+import requests.cookies
 
 # Import Objects
 from .initialize import initializer
@@ -12,18 +13,14 @@ from .initialize import initializer
 
 class Requester:
     def __init__(self):
-        self.response = None
+        self.response = requests.Response()
         self.url = None
         self.data = None
         self.req = None
         self.htmltext = None
         self.status_code = None
         self.headers = initializer.headers
-        self.ctx = ssl.create_default_context()
-        self.ctx.check_hostname = False
-        self.ctx.verify_mode = ssl.CERT_NONE
-        self.cookieJar = http.cookiejar.CookieJar()
-        self.cookieHandler = urllib.request.HTTPCookieProcessor(self.cookieJar)
+        self.cookieJar = requests.cookies.RequestsCookieJar()
 
     def request(self, url, data):
         self.url = parse.quote_plus(url, safe=':/')
@@ -32,24 +29,18 @@ class Requester:
             data = urllib.parse.urlencode(data)
         if data:
             data = data.encode('utf-8')
-        self.req = urllib.request.Request(url=url, data=data, headers=self.headers)
-        urllib.request.install_opener(urllib.request.build_opener())
         try:
-            # Returns 200
             if initializer.nosslcheck:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'), context=self.ctx)
+                self.response = requests.get(url=self.url, data=data, headers=self.headers, verify=False)
             else:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'))
-            if self.response.status != 200:
-                raise urllib.error.HTTPError
-            # it will ignore any bad character without replacing it
-            self.htmltext = self.response.read().decode('utf-8', 'ignore')
-            self.status_code = self.response.status
-        except urllib.error.HTTPError or urllib.error.URLError as e:
-            # Does not return  200
-            self.response = e
-            self.htmltext = e.read().decode('utf-8', 'ignore')
-            self.status_code = e.code
+                self.response = requests.get(url=self.url, data=data, headers=self.headers)
+            self.response.raise_for_status()
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
+        except requests.RequestException as e:
+            self.response = e.response
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
 
     def noredirect(self, url, data):
         self.url = parse.quote_plus(url, safe=':/')
@@ -58,23 +49,18 @@ class Requester:
             data = urllib.parse.urlencode(data)
         if data:
             data = data.encode('utf-8')
-        self.req = urllib.request.Request(url=url, data=data, headers=self.headers)
-        urllib.request.install_opener(urllib.request.build_opener(NoRedirects()))
         try:
-            # Returns 200
             if initializer.nosslcheck:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'), context=self.ctx)
+                self.response = requests.get(url=self.url, data=data, headers=self.headers, verify=False, allow_redirects=False)
             else:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'))
-            if self.response.status != 200:
-                raise urllib.error.HTTPError
-            self.htmltext = self.response.read().decode('utf-8', 'ignore')
-            self.status_code = self.response.status
-        except urllib.error.HTTPError or urllib.error.URLError as e:
-            # Does not return  200
-            self.response = e
-            self.htmltext = e.read().decode('utf-8', 'ignore')
-            self.status_code = e.code
+                self.response = requests.get(url=self.url, data=data, headers=self.headers, allow_redirects=False)
+            self.response.raise_for_status()
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
+        except requests.RequestException as e:
+            self.response = e.response
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
 
     def requestcookie(self, url, data):
         self.url = parse.quote_plus(url, safe=':/')
@@ -83,32 +69,18 @@ class Requester:
             data = urllib.parse.urlencode(data)
         if data:
             data = data.encode('utf-8')
-        self.req = urllib.request.Request(url=url, data=data, headers=self.headers)
-        urllib.request.install_opener(urllib.request.build_opener(self.cookieHandler))
         try:
-            # Returns 200
             if initializer.nosslcheck:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'), context=self.ctx)
+                self.response = requests.get(url=self.url, data=data, headers=self.headers, cookies=self.cookieJar, verify=False)
             else:
-                self.response = urllib.request.urlopen(url=parse.quote_plus(self.req.get_full_url(), safe=':/'))
-            if self.response.status != 200:
-                raise urllib.error.HTTPError
-            # it will ignore any bad character without replacing it
-            self.htmltext = self.response.read().decode('utf-8', 'ignore')
-            self.status_code = self.response.status
-        except urllib.error.HTTPError or urllib.error.URLError as e:
-            # Does not return  200
-            self.response = e
-            self.htmltext = e.read().decode('utf-8', 'ignore')
-            self.status_code = e.code
-
-
-class NoRedirects(urllib.request.HTTPRedirectHandler):
-    # Redirect handler that simply raises a Redirect() for all http_error_30*() methods
-    def redirect_request(self, req, fp, code, msg, headers, newurl):
-        RedirError = urllib.error.HTTPError(req.get_full_url(), code, msg, headers, fp)
-        RedirError.status = code
-        raise RedirError
+                self.response = requests.get(url=self.url, data=data, headers=self.headers, cookies=self.cookieJar)
+            self.response.raise_for_status()
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
+        except requests.RequestException as e:
+            self.response = e.response
+            self.htmltext = self.response.text
+            self.status_code = self.response.status_code
 
 
 requester = Requester()
